93fbd758723fd8fbc289a8c69a8005eca0c2b013
KnightHarute <knight_harute@foxmail.com>
2020-11-16 09:39:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pickle
 import sklearn
 import numpy as np
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "CSCI626"
 # use timedelta to let the session store data for 2 days or 30 mins
-app.permanent_session_lifetime = timedelta(minutes=1)
+app.permanent_session_lifetime = timedelta(minutes=2)
 
 @app.route("/")
 def home():
@@ -31,22 +31,29 @@ def login():
         session["review"] = {'Category': category,
                             'Review': review,
                             'Predict rating': int(review_rating[0])}
+        flash("Input Review Successful!")
         return redirect(url_for("review"))
     else:
         if "review" in session:
+            flash("Already predicted a review!\nClick button for new prediction")
             return redirect(url_for("review"))
         return render_template("login.html")
 @app.route("/review")
 def review():
     if "review" in session:
         review = session["review"]
-        return f"<h1>Category:{review.get('Category')}</h1>\n<h2>Review:{review.get('Review')}</h2>\n<h1>Rating:{review.get('Predict rating')}</h1>"
-    else:
+        return render_template("review.html", review=review)
 
+    else:
+        flash("Already predicted a review!\nClick button for new prediction")
         return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
+    # if "review" in session:
+    #     review = session["review"]
+    # flash(f"<h2>Clearing the old review:{review.get('Review')}</h2>\n<h2>Prepare for another review</h2>", "info")
+    flash("CLEAR, Ready to Predict another review", "info")
     session.pop("review", None)
     return redirect(url_for("login"))
 
