971b524366365b46eecdd3ca5c6b15642688747f
KnightHarute <knight_harute@foxmail.com>
2020-11-18 10:25:24 -0500

    Add files via upload
    known error in dev_test_error branch in "login3"
    yelp-review-api-01-9ba27b4ac961.json is the credentials file, but I don't know how to use it

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pickle
 import sklearn
 import numpy as np
+from google.cloud import storage
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+import os
+
 
 app = Flask(__name__)
 app.secret_key = "CSCI626"
 # use timedelta to let the session store data for 2 days or 30 mins
-app.permanent_session_lifetime = timedelta(minutes=2)
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
@@ -38,6 +41,86 @@ def login():
             flash("Already predicted a review!\nClick button for new prediction")
             return redirect(url_for("review"))
         return render_template("login.html")
+
+
+@app.route("/login2", methods=["POST"])
+def login2():
+    '''
+    test:
+
+    {
+        "Category": "Pets",
+        "Review":"nice pet for me "
+    }
+    '''
+    if request.method == "POST":
+        session.permanent = True
+        json_data = request.get_json()
+        # {'Category': 'Pets', 'review': 'the pets grommers are h'}
+        print(json_data)
+        category = json_data['Category'].replace(" ", "_")
+        review = json_data['Review']
+
+        # predict
+        filename = category + "_yelp_review_predict_star.pkl"
+        with open(filename, 'rb') as infile:
+            model = pickle.load(infile)
+            review_rating = model.predict([review])
+
+        session["review"] = {'Category': category,
+                             'Review': review,
+                             'Predict rating': int(review_rating[0])}
+        flash("Input Review Successful!")
+
+        return session["review"]
+    else:
+        if "review" in session:
+            flash("Already predicted a review!\nClick button for new prediction")
+            return redirect(url_for("review"))
+        return render_template("login.html")
+
+@app.route("/login3", methods=["POST","GET"])
+def login3():
+    if request.method == "POST":
+        session.permanent = True
+        category = request.form['cate']
+        review = request.form["rv"]
+
+        # #predict
+        # category = category.replace(" ", "_")
+        # filename = category + "_yelp_review_predict_star.pkl"
+        # infile = open(filename, "rb")
+        # model = pickle.load(infile)
+        # infile.close()
+        # review_rating = model.predict([review])
+        # predict-2
+        bucket_name = 'yelp-review-api-01.appspot.com'
+        file_path = 'Pets_yelp_review_predict_star.pkl'
+        client = storage.Client()
+        #storage_client = storage.Client()
+        bucket = client.get_bucket(bucket_name)
+        #bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(file_path)
+        blob.download_to_filename('download.pkl')
+
+        with open('download.pkl', 'rb') as f:
+            model = pickle.load(f)
+            review_rating = model.predict([review])
+
+        session["review"] = {'Category': category,
+                            'Review': review,
+                            'Predict rating': int(review_rating[0])}
+        flash("Input Review Successful!")
+        os.remove('download.pkl')
+        return redirect(url_for("review"))
+    else:
+        if "review" in session:
+            flash("Already predicted a review!\nClick button for new prediction")
+            return redirect(url_for("review"))
+        return render_template("login.html")
+
+
+
 @app.route("/review")
 def review():
     if "review" in session:
