8e1e033513cd8bec68309f961cf187353b8c2ad5
KnightHarute <knight_harute@foxmail.com>
2020-12-04 02:23:40 -0500

    vue front for 149.28.91.13

--- main.py
+++ main.py
@@ -1,77 +1,36 @@
 import pickle
 import sklearn
 import numpy as np
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask, redirect, url_for, render_template, request, session, flash, jsonify
 from datetime import timedelta
 
+app = Flask(__name__,
+            static_folder="./static",
+            template_folder="./")
 
-app = Flask(__name__)
 app.secret_key = "CSCI"
 # use timedelta to let the session store data for 2 days or 30 mins
 app.permanent_session_lifetime = timedelta(minutes=5)
 
-@app.route("/")
-def home():
+
+@app.route('/')
+def index():
     return render_template("index.html")
-    #return render_template("index.html", content=name, r=2)
-
-@app.route("/predictor", methods=["POST","GET"])
-def predictor():
-    if request.method == "POST":
-        session.permanent = True
-        category = request.form['cate']
-        review = request.form["rv"]
-        #predict
-        category = category.replace(" ", "_")
-        filename = category + "_yelp_review_predict_star.pkl"
-        infile = open(filename, "rb")
-        model = pickle.load(infile)
-        infile.close()
-        review_rating = model.predict([review])
-
-        session["review"] = {'Category': category,
-                            'Review': review,
-                            'Predict rating': int(review_rating[0])}
-        flash("Input Review Successful!")
-        return redirect(url_for("review"))
-    else:
-        if "review" in session:
-            flash("Already predicted a review!\nClick button for new prediction")
-            return redirect(url_for("review"))
-        return render_template("predictor.html")
 
 
+@app.route("/sub", methods=["POST", "GET"])
+def sub():
+    print(request.get_json(silent=True))
+    category = request.get_json(silent=True).get("category")
+    category = category.replace(" ", "_")
+    review = request.get_json(silent=True).get("review")
+    filename = category + "_yelp_review_predict_star.pkl"
+    infile = open(filename, "rb")
+    model = pickle.load(infile)
+    infile.close()
+    review_rating = model.predict([review])
+    return jsonify(int(review_rating[0]))
 
-
-@app.route("/review")
-def review():
-    if "review" in session:
-        review = session["review"]
-        return render_template("review.html", review=review)
-
-    else:
-        flash("Already predicted a review!\nClick button for new prediction")
-        return redirect(url_for("predictor"))
-
-@app.route("/clear")
-def clear():
-    # if "review" in session:
-    #     review = session["review"]
-    # flash(f"<h2>Clearing the old review:{review.get('Review')}</h2>\n<h2>Prepare for another review</h2>", "info")
-    flash("CLEAR, Ready to Predict another review", "info")
-    session.pop("review", None)
-    return redirect(url_for("predictor"))
-
-
-# @app.route("/<name>")
-# def user(name):
-#     return f"Hello {name}!"
-#
-# @app.route("/admin")
-# def admin():
-#     return redirect(url_for("user", name="Admin!"))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
-
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
